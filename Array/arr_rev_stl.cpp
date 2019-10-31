4eb5675d1c83b5092c191a1a6a60108f8a39b0cd
Akash Mondal <a98mondal@gmail.com>
2019-10-31 21:58:38 +0530

    Array: add a virtual array reverse function
    * with this inline func you can get the effect of reversed array
      without actually reversing it.
    
    Signed-off-by: Akash Mondal <a98mondal@gmail.com>

--- Array/arr_rev_stl.cpp
+++ Array/arr_rev_stl.cpp
@@ -1,19 +1,28 @@
-#include<bits/stdc++.h> 
-using namespace std; 
-  
+#include<bits/stdc++.h>
+
+inline int arr_rev(int* arr, int pos, int len) {
+    return arr[len - pos -1];
+}
+
+using namespace std;
 int main(){
-    int arr[] = { 19, 55, 51, 59, 56, 32 };
+    int arr[] = { 19, 55, 51, 59, 56, 32, 67, 22, 24 };
     int n = sizeof(arr) / sizeof(arr[0]);
     
 	cout << "Array before Reversal: "; 
     for (int i = 0; i < n; i++) 
         cout << arr[i] << " ";
+
+    cout << "\nVirtually Reversed Array: "; 
+    for (int i = 0; i < n; i++) 
+        cout << arr_rev(arr, i, n) << " ";
     
 	reverse(arr, arr + n);
 	
     cout << "\nReversed Array: "; 
     for (int i = 0; i < n; i++) 
-        cout << arr[i] << " "; 
-	
+        cout << arr[i] << " ";
+
+    cout << endl;
     return 0; 
 } 
